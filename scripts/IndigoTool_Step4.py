78e450923469b495a8858e16f063ce1e0a883c2c
Christian Matthews <35038843+csmatthews@users.noreply.github.com>
2021-09-17 15:22:32 +0000

    Update IndigoTool_Step4.py

--- scripts/IndigoTool_Step4.py
+++ scripts/IndigoTool_Step4.py
@@ -8,14 +8,14 @@ Created on Mon Aug  9 09:25:32 2021
 #Run in QGIS
 
 processing.run("native:joinattributesbylocation", 
-{'INPUT': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'ccubed\' sslmode=disable key=\'gid\' srid=102005 type=MultiPolygon checkPrimaryKeyUnicity=\'1\' table=\"public\".\"facilities_buf25mi\" (geom)',
-'JOIN': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'ccubed\' sslmode=disable key=\'gid\' srid=4326 type=MultiPolygon checkPrimaryKeyUnicity=\'1\' table=\"public\".\"counties\" (geom)',
+{'INPUT': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'\' sslmode=disable key=\'gid\' srid=102005 type=MultiPolygon checkPrimaryKeyUnicity=\'1\' table=\"public\".\"facilities_buf25mi\" (geom)',
+'JOIN': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'\' sslmode=disable key=\'gid\' srid=4326 type=MultiPolygon checkPrimaryKeyUnicity=\'1\' table=\"public\".\"counties\" (geom)',
 'PREDICATE':[0],
 'JOIN_FIELDS':['total2020value'],
 'METHOD':0,
 'DISCARD_NONMATCHING':False,
 'PREFIX':'',
-'OUTPUT': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'ccubed\' sslmode=disable table=\"public\".\"facilities_buf25mi_joincounties\" (geom)'})
+'OUTPUT': 'postgres://dbname=\'indigo\' host=localhost port=5432 user=\'postgres\' password=\'\' sslmode=disable table=\"public\".\"facilities_buf25mi_joincounties\" (geom)'})
 
 ### Run Step 5 "C:\Users\cmatthews\Desktop\Indigo\Tool\IndigoTool_Step3.py"
 print("Move onto Run IndigoTool_Step5.py")
